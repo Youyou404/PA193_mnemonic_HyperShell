5e500c9bcb3525e24452b49fa56816e3a15eadf6
Yorick Kupczyk <kupczyk.y@gmail.com>
2019-10-29 12:49:13 +0100

    Added a test to check the given mnemonic
    The mnemonic passes as an argument to generate_seed is now checked against a wordlist

--- seed/seed.py
+++ seed/seed.py
@@ -18,18 +18,24 @@ def get_wordlist(filepath):
 
         return wordlist
 
-def generate_seed(mnemonic, passphrase=''):
+def generate_seed(mnemonic, passphrase='', filepath='../wordlists/english.txt'):
     """
     Get the seed from a mnemonic
     :param mnemonic: mnemonic as a string
     :param passphrase: passphrase as a string
+    :param filepath: path to the used wordlist
     :return: seed as a hex string
     """
-    nfkd_mnemonic = bytes(unicodedata.normalize('NFKD', mnemonic), encoding='utf-8')
 
+    wordlist = get_wordlist(filepath)
+    splited_mnemonic = mnemonic.split()
+    for i in range(0,len(splited_mnemonic)):
+        if(splited_mnemonic[i] not in wordlist):
+            raise ValueError("the given mnemonic contains illegal(s) word(s)")
+
+    nfkd_mnemonic = bytes(unicodedata.normalize('NFKD', mnemonic), encoding='utf-8')
     concat = 'mnemonic' + passphrase
     nfkd_salt = bytes(unicodedata.normalize('NFKD', concat), encoding='utf-8')
-
     seed = PBKDF2(nfkd_mnemonic, nfkd_salt, 2048, macmodule=hmac, digestmodule=sha512).read(64).hex()
 
     return seed

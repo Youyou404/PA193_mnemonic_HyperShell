491b10d6ffdd2bf3073a0510120585fd6e02814e
Yorick Kupczyk <kupczyk.y@gmail.com>
2019-10-29 12:37:18 +0100

    Added the get_wordlist function to seed.py
    Unable to import a function from the mnemonic lib. This commit is just a fix, need to find another way to import an already existing function

--- seed/seed.py
+++ seed/seed.py
@@ -3,6 +3,21 @@ import hmac
 import unicodedata
 from pbkdf2 import PBKDF2
 
+def get_wordlist(filepath):
+    """
+    Get list of words from the file. Each line is interpreted as a word (except the possibly empty last line).
+    """
+    with open(filepath, "r") as f:
+        wordlist = f.read().split("\n")
+        if wordlist[-1] == "":
+            wordlist.pop(-1)
+
+        if len(wordlist) != 2048:
+            raise ValueError("the filepath contains {} lines interpreted as words; "
+                             "it should contain 2048 words".format(len(wordlist)))
+
+        return wordlist
+
 def generate_seed(mnemonic, passphrase='TREZOR'):
     """
     Get the seed from a mnemonic

f5285d47e69b4382cac597540e6c7db73a6f8221
Yorick Kupczyk <kupczyk.y@gmail.com>
2019-10-30 11:36:05 +0100

    Added seed part for the ui

--- mnemonic-ui/ui.py
+++ mnemonic-ui/ui.py
@@ -1,6 +1,7 @@
 import argparse
 from mnemonic.mnemonic import POSSIBLE_ENT_VALUES, generate_entropy_bytes, get_mnemonic_from_entropy_bytes, \
     reverse_mnemonic
+from mnemonic.seed import generate_seed
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='User interface showcasing the mnemonics library.')
@@ -20,8 +21,12 @@ if __name__ == '__main__':
     parser_mnemonic.add_argument('-wl', dest='mnemonic_wordlist_path', help='wordlist file path', metavar='path')
 
     # seed sub parser
-    # TODO
     parser_seed = subparsers.add_parser('seed', help='seed functionality')
+    parser_seed.add_argument('-e', dest='seed_entropy', help='entropy', metavar='entropy')
+    parser_seed.add_argument('-mn', dest='seed_mnemonic', help='mnemonic', metavar='mnemonic')
+    parser_seed.add_argument('-s', dest='seed_seed', help='seed', metavar='seed')
+    parser_seed.add_argument('-wl', dest='seed_wordlist', help='wordlist file path', metavar='path')
+    parser_seed.add_argument('-p', dest='seed_passphrase', help='passphrase', metavar='passphrase')
 
     args = parser.parse_args()
     # entropy functionality
@@ -56,10 +61,37 @@ if __name__ == '__main__':
         print(mnemonic)
     # seed functionality
     elif args.command == 'seed':
-        # TODO generate seed from entropy
-        # TODO generate seed from mnemonic
-        # TODO check given mnemonic generates given seed
-        pass
+        # process the wordlist
+        if args.seed_wordlist is None:
+            wordlist_path = '../wordlists/english.txt'
+        else:
+            wordlist_path = args.seed_wordlist
+
+        # verify a given seed against a given entropy
+        if args.seed_seed is not None and args.seed_entropy is not None and args.seed_mnemonic is None:
+            mnemonic = get_mnemonic_from_entropy_bytes(args.seed_entropy, wordlist_path)
+            seed = generate_seed(mnemonic, args.seed_passphrase, wordlist_path)
+            if seed == args.seed_seed:
+                print("The given entropy match the given seed")
+            else:
+                print("The given entropy does not match the given seed")
+        # verify a given seed seed against a given mnemonic
+        elif args.seed_seed is not None and args.seed_mnemonic is not None and args.seed_entropy is None:
+            seed = generate_seed(args.seed_mnemonic, args.seed_passphrase, wordlist_path)
+            if seed == args.seed_seed:
+                print("The given mnemonic match the given seed")
+            else:
+                print("The given mnemonic does not match the given seed")
+        # generate a seed from a given entropy
+        elif args.seed_entropy is not None and args.seed_mnemonic is None and args.seed_seed is None:
+            mnemonic = get_mnemonic_from_entropy_bytes(args.seed_entropy, wordlist_path)
+            seed = generate_seed(mnemonic, args.seed_passphrase, wordlist_path)
+            print(seed)
+        # generate seed from a given mnemonic
+        elif args.seed_mnemonic is not None and args.seed_entropy is None and args.seed_seed is None:
+            seed = generate_seed(args.seed_mnemonic, args.seed_passphrase, wordlist_path)
+            print(seed)
+
     # print help
     else:
         parser.parse_args(['--help'])
